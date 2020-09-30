b0ab07aa36bc305b7e6a85f9d04d3881aff1a5aa
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-09-30 09:52:04 -0500

    feat(create-emp) add new-emp form

--- react-crud-app/src/components/ListEmployeeComponent.jsx
+++ react-crud-app/src/components/ListEmployeeComponent.jsx
@@ -7,6 +7,11 @@ class ListEmployeeComponent extends Component {
     this.state = {
       employees: [],
     };
+    this.addEmployee = this.addEmployee.bind(this);
+  }
+
+  addEmployee() {
+    this.props.history.push("/add-employee");
   }
 
   componentDidMount() {
@@ -18,6 +23,12 @@ class ListEmployeeComponent extends Component {
     return (
       <div>
         <h2 className="text-center">Employees List</h2>
+        <div className="row">
+          <button className="btn btn-primary" onClick={this.addEmployee}>
+            {" "}
+            Add Employee
+          </button>
+        </div>
         <div className="row">
           <table className="table table-striped table-bordered">
             <thead>
