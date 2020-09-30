2335aa5496e722843b4dc569ff97b6816ebb9c1e
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-09-30 09:03:48 -0500

    feat(emp-disp) add routing feature

--- react-crud-app/src/App.js
+++ react-crud-app/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" component={ListEmployeeComponent}></Route>
+            <Route path="/employees" component={ListEmployeeComponent}></Route>
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
