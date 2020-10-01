ee2a7ff09be49623c4eb77ad4e0dff8018023715
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-10-01 16:57:59 -0500

    Added TableScrollbar

--- react-crud-app/src/components/ListEmployeeComponent.jsx
+++ react-crud-app/src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import EmployeeService from "../services/EmployeeService";
+import TableScrollbar from "react-table-scrollbar";
 
 class ListEmployeeComponent extends Component {
   constructor(props) {
@@ -51,48 +52,50 @@ class ListEmployeeComponent extends Component {
           </button>
         </div>
         <br></br>
-        <div className="row">
-          <table className="table table-striped table-bordered">
-            <thead>
-              <tr>
-                <th> Employee First Name</th>
-                <th> Employee Last Name</th>
-                <th> Employee Email Id</th>
-                <th> Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.employees.map((employee) => (
-                <tr key={employee.id}>
-                  <td> {employee.firstName} </td>
-                  <td> {employee.lastName}</td>
-                  <td> {employee.emailId}</td>
-                  <td>
-                    <button
-                      onClick={() => this.editEmployee(employee.id)}
-                      className="btn btn-info"
-                    >
-                      Update{" "}
-                    </button>
-                    <button
-                      style={{ marginLeft: "10px" }}
-                      onClick={() => this.deleteEmployee(employee.id)}
-                      className="btn btn-danger"
-                    >
-                      Delete{" "}
-                    </button>
-                    <button
-                      style={{ marginLeft: "10px" }}
-                      onClick={() => this.viewEmployee(employee.id)}
-                      className="btn btn-info"
-                    >
-                      View{" "}
-                    </button>
-                  </td>
+        <div style={{ height: "30vh" }}>
+          <TableScrollbar rows={7}>
+            <table className="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th> Employee First Name</th>
+                  <th> Employee Last Name</th>
+                  <th> Employee Email Id</th>
+                  <th> Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {this.state.employees.map((employee) => (
+                  <tr key={employee.id}>
+                    <td> {employee.firstName} </td>
+                    <td> {employee.lastName}</td>
+                    <td> {employee.emailId}</td>
+                    <td>
+                      <button
+                        onClick={() => this.editEmployee(employee.id)}
+                        className="btn btn-info"
+                      >
+                        Update{" "}
+                      </button>
+                      <button
+                        style={{ marginLeft: "10px" }}
+                        onClick={() => this.deleteEmployee(employee.id)}
+                        className="btn btn-danger"
+                      >
+                        Delete{" "}
+                      </button>
+                      <button
+                        style={{ marginLeft: "10px" }}
+                        onClick={() => this.viewEmployee(employee.id)}
+                        className="btn btn-info"
+                      >
+                        View{" "}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </TableScrollbar>
         </div>
       </div>
     );
