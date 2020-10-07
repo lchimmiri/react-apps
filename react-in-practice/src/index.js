ccfef29da3e249ef882f53c5646b98a16c96913b
Naidu Chimmiri <chimmiri.naidu@gmail.com>
2020-10-06 19:22:08 -0500

    props as object

--- react-in-practice/src/index.js
+++ react-in-practice/src/index.js
@@ -1,41 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const showDate = (time) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const month = months[time.getMonth()].slice(0, 3);
+  const year = time.getFullYear();
+  const date = time.getDate();
+  return ` ${month} ${date}, ${year}`;
+};
 // Header Component
-const Header = (props) => (
-  <header>
-    <div className="header-wrapper">
-      <h1>{props.welcome}</h1>
-      <h2>{props.title}</h2>
-      <h3>{props.subtitle}</h3>
-      <p>
-        {props.firstName} {props.lastName}
-      </p>
-      <small>{props.date}</small>
-    </div>
-  </header>
-);
+const Header = (props) => {
+  return (
+    <header>
+      <div className="header-wrapper">
+        <h1>{props.data.welcome}</h1>
+        <h2>{props.data.title}</h2>
+        <h3>{props.data.subtitle}</h3>
+        <p>
+          {props.data.author.firstName} {props.data.author.lastName}
+        </p>
+        <small>{showDate(props.data.date)}</small>
+      </div>
+    </header>
+  );
+};
 
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
-  const welcome = "Welcome to 30 Days Of React";
-  const title = "Getting Started React";
-  const subtitle = "JavaScript Library";
-  const firstName = "Asabeneh";
-  const lastName = "Yetayeh";
-  const date = "Oct 4, 2020";
+  const data = {
+    welcome: "Welcome to 30 Days Of React",
+    title: "Getting Started React",
+    subtitle: "JavaScript Library",
+    author: {
+      firstName: "Asabeneh",
+      lastName: "Yetayeh",
+    },
+    date: new Date(), // date needs to be formatted to a human readable format
+  };
 
   return (
     <div className="app">
-      <Header
-        welcome={welcome}
-        title={title}
-        subtitle={subtitle}
-        firstName={firstName}
-        lastName={lastName}
-        date={date}
-      />
+      <Header data={data} />
     </div>
   );
 };
